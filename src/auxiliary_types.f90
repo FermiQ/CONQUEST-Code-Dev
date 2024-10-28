6cad79f9df0e143ba402b71f4f09e6bbfc4b7857
FermiQ <bishnoi@ieee.org>
2024-10-28 10:26:38 +0100

    CONQUEST-Code-Dev-CodeLLM

--- src/auxiliary_types.f90
+++ src/auxiliary_types.f90
@@ -11,7 +11,7 @@
 ! NAME
 !  auxiliary_types
 ! PURPOSE
-!  
+!  This module defines auxiliary data types used in the Conquest code.
 ! AUTHOR
 !  M.Arita
 ! CREATION DATE
@@ -20,29 +20,29 @@
 ! SOURCE
 !
 module auxiliary_types
-  ! Module usage
+  ! Use statement for the datatypes module.  Presumably defines basic data types used here.
   use datatypes
   implicit none
 
-  !****s* auxiliary_types/
+  !****s* auxiliary_types/group_aux
   ! NAME
   !  group_aux
   ! PURPOSE
-  !  Define all variables related to auxiliary groups
+  !  This derived type defines all variables related to auxiliary groups of atoms.  This likely facilitates grouping atoms for specific calculations or analysis.
   ! AUTHOR
   ! SOURCE
   !
   type group_aux
-   ! Schalor
-   integer :: n_grp                        ! No. of groups
-   character(20) :: filename               ! auxiliary file name
-   ! Arrays
-   character(20),pointer :: grp_name(:)    ! group name
-   integer,pointer :: n_atom_in_grp(:)     ! No. of atoms composing group
-   integer,pointer :: n_subgrp(:)          ! no. of subgroups for each group
-   integer,pointer :: iatom_beg(:)         ! initial address for glob_atom w.r.t subgroups
-   integer,pointer :: ibeg_grp(:)          ! initial address for glob_atom w.r.t groups
-   integer,pointer :: glob_atom(:)         ! global labels of the atoms in the group
+   ! Scalars:  These variables store single values.
+   integer :: n_grp                        ! No. of groups defined.
+   character(20) :: filename               ! Name of the file containing auxiliary group information.
+   ! Arrays: These variables are arrays, the size of which is determined dynamically.  The use of pointers allows for flexible memory allocation.
+   character(20),pointer :: grp_name(:)    ! Array of group names (strings of length 20).
+   integer,pointer :: n_atom_in_grp(:)     ! Array containing the number of atoms in each group.
+   integer,pointer :: n_subgrp(:)          ! Array containing the number of subgroups within each group.
+   integer,pointer :: iatom_beg(:)         ! Array indicating the starting index of atoms for each subgroup within the glob_atom array.
+   integer,pointer :: ibeg_grp(:)          ! Array indicating the starting index of atoms for each group within the glob_atom array.
+   integer,pointer :: glob_atom(:)         ! Array containing the global indices of all atoms belonging to the groups.
   end type group_aux
   
 end module auxiliary_types
